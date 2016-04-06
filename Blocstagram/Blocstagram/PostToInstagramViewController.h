56c708f98ee7cbc23f6e060cb6d95d26a32985ed
Daniel Rodas <shadylane318@me.com>
2016-04-05 22:46:40 -0400

    Checkpoint 43 Completed

---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Daniel Rodas on 4/5/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
